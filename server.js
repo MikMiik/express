94e4517386b0f9f02e23a0eafeb827e93c2ddb5c
MikMik <minh0936532430@gmail.com>
2025-04-29 09:54:47 +0700

    Add CORS

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const router = require("./routes");
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
